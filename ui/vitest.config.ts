d31797baa7c2cac7a5813678868e06d35d855c71
yamid-granda <vallenatomanizales@gmial.com>
2024-08-12 10:24:45 -0500

    chore: unit test dev environment

---
+++ ui/vitest.config.ts
@@ -0,0 +1,16 @@
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
+  },
+  resolve: {
+    alias: [
+      { find: '@/ui', replacement: path.resolve(__dirname, './src/') },
+    ],
+  },
+})
