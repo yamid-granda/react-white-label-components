19132dccde2a519e3b1de045642815137a6fa5c7
yamid-granda <vallenatomanizales@gmial.com>
2024-08-13 22:33:36 -0500

    feat: improve color thesis

--- ui/src/examples/ColorThesis/index.tsx
+++ ui/src/examples/ColorThesis/index.tsx
@@ -4,21 +4,31 @@ export function ColorThesis() {
   return (
     <div className="wl-color-thesis">
       <div className="wl-color-thesis__light">
-        <div className="wl-color-thesis__white">White - Light Theme</div>
-        <div className="wl-color-thesis__grey-8">Grey 8</div>
-        <div className="wl-color-thesis__grey-6">Grey 6</div>
-        <div className="wl-color-thesis__grey-4">Grey 4</div>
-        <div className="wl-color-thesis__grey-2">Grey 2</div>
-        <div className="wl-color-thesis__black">Black</div>
+        <h2>Light Theme</h2>
+        <div className="wl-color-thesis__example wl-color-thesis__white">
+          White -
+          {' '}
+          <span className="wl-color-thesis__light-text">light text</span>
+        </div>
+        <div className="wl-color-thesis__example wl-color-thesis__grey-8">Grey 8</div>
+        <div className="wl-color-thesis__example wl-color-thesis__grey-6">Grey 6 - Page Background</div>
+        <div className="wl-color-thesis__example wl-color-thesis__grey-4">Grey 4 - Border Color</div>
+        <div className="wl-color-thesis__example wl-color-thesis__black">Black</div>
+        <div className="wl-color-thesis__example wl-color-thesis__primary">Primary</div>
       </div>
 
       <div className="wl-color-thesis__dark">
-        <div className="wl-color-thesis__white">White - Dark Theme</div>
-        <div className="wl-color-thesis__grey-8">Grey 8</div>
-        <div className="wl-color-thesis__grey-6">Grey 6</div>
-        <div className="wl-color-thesis__grey-4">Grey 4</div>
-        <div className="wl-color-thesis__grey-2">Grey 2</div>
-        <div className="wl-color-thesis__black">Black</div>
+        <h2>Dark Theme</h2>
+        <div className="wl-color-thesis__example wl-color-thesis__white">
+          White -
+          {' '}
+          <span className="wl-color-thesis__light-text">light text</span>
+        </div>
+        <div className="wl-color-thesis__example wl-color-thesis__grey-8">Grey 8</div>
+        <div className="wl-color-thesis__example wl-color-thesis__grey-6">Grey 6 - Page Background</div>
+        <div className="wl-color-thesis__example wl-color-thesis__grey-4">Grey 4 - Border Color</div>
+        <div className="wl-color-thesis__example wl-color-thesis__black">Black</div>
+        <div className="wl-color-thesis__example wl-color-thesis__primary">Primary</div>
       </div>
     </div>
   )
---
+++ ui/src/examples/ColorThesis/stories/index.stories.ts
@@ -0,0 +1,12 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { ColorThesis } from '../index'
+
+const meta: Meta<typeof ColorThesis> = {
+  component: ColorThesis,
+}
+
+export default meta
+type Story = StoryObj<typeof ColorThesis>
+
+export const Example: Story = {}
